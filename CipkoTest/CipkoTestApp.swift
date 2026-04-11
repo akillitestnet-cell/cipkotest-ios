eb1468aaa9f122c7139eed1972432aa8a8accc7f
akillitestnet-cell <akillitest.net@gmail.com>
2026-04-11 23:01:10 +0300

    Update CipkoTestApp.swift

--- CipkoTest/CipkoTestApp.swift
+++ CipkoTest/CipkoTestApp.swift
@@ -3,13 +3,21 @@ import RevenueCat
 
 @main
 struct CipkoTestApp: App {
-    init() {
-        Purchases.configure(withAPIKey: "test_JCtkbdjsQjajehffxGNairKVvRf")
-    }
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        Purchases.configure(withAPIKey: "test_JCtkbdjsQjajehffxGNairKVvRf")
+        return true
     }
 }
