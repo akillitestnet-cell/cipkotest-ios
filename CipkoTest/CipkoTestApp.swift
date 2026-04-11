258de2dca8dd3723a56edb60f92e43bb7ba14479
akillitestnet-cell <akillitest.net@gmail.com>
2026-04-11 23:26:09 +0300

    Update CipkoTestApp.swift

--- CipkoTest/CipkoTestApp.swift
+++ CipkoTest/CipkoTestApp.swift
@@ -14,10 +14,14 @@ struct CipkoTestApp: App {
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
-    ) -> Bool {
-        Purchases.configure(withAPIKey: "test_JCtkbdjsQjajehffxGNairKVvRf")
-        return true
-    }
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+) -> Bool {
+    #if DEBUG
+    Purchases.configure(withAPIKey: "test_JCtkbdjsQjajehffxGNairKVvRf")
+    #else
+    Purchases.configure(withAPIKey: "appl_VEYSmwGXvnAecuKCApDFNBUVpqk")
+    #endif
+    return true
+}
 }
