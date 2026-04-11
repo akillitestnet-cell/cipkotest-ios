87d0ec843aa270f4f85c3536c5513e3e50d47977
akillitestnet-cell <akillitest.net@gmail.com>
2026-04-11 19:21:38 +0300

    Update CipkoTestApp.swift

--- CipkoTest/CipkoTestApp.swift
+++ CipkoTest/CipkoTestApp.swift
@@ -1,7 +1,12 @@
 import SwiftUI
+import RevenueCat
 
 @main
 struct CipkoTestApp: App {
+    init() {
+        Purchases.configure(withAPIKey: "test_JCtkbdjsQjajehffxGNairKVvRf")
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
